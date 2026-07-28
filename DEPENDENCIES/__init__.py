4e5bc77b1d8892eb82c3361aee2c03c2447508ea
Samuel Hironimus <samuel.p.hironimus@gmail.com>
2026-07-28 17:29:20 -0300

    fix: ajusta __init__ conforme a main

--- DEPENDENCIES/__init__.py
+++ DEPENDENCIES/__init__.py
@@ -1,9 +1,10 @@
 from .database import SessionLocal
 from .session import pegar_sessao
-from .auth import usuario_logado
+from .auth import verificar_token_oauth, verificar_token
 
 __all__ = [
     SessionLocal
     ,pegar_sessao
-    ,usuario_logado
+    ,verificar_token_oauth
+    ,verificar_token
 ]
