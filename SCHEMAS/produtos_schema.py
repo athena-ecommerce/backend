cbd692e0996a6efde7cc7a13fe2c530585787b14
Lorenzo Lima de Oliveira <lorenzolimadeoliveira2010@gmail.com>
2026-08-11 00:21:07 -0300

    feat: Adidiciona funcionanalidade para adicionar imagem

--- SCHEMAS/produtos_schema.py
+++ SCHEMAS/produtos_schema.py
@@ -1,31 +1,70 @@
 from pydantic import BaseModel, ConfigDict, Field
-
+from fastapi import UploadFile, File, Form
 
 class ArteCadastro(BaseModel):
 
     nome: str
     tipo_arte: str
     preco: float = Field(gt=0)
+    imagem: UploadFile
 
     model_config = ConfigDict(
-        from_attributes=True,
+        arbitrary_types_allowed=True,
         extra="forbid",
         str_strip_whitespace=True,
     )
 
+    @classmethod
+    def as_form(
+        cls,
+        nome: str = Form(...),
+        tipo_arte: str = Form(...),
+        preco: float = Form(...),
+        imagem: UploadFile = File(...),
+    ):
+        return cls(
+            nome=nome,
+            tipo_arte=tipo_arte,
+            preco=preco,
+            imagem=imagem,
+        )
 
 class ArteAtualizar(BaseModel):
 
     nome: str
     tipo_arte: str
     preco: float = Field(gt=0)
+    imagem: UploadFile
 
     model_config = ConfigDict(
-        from_attributes=True,
+        arbitrary_types_allowed=True,
         extra="forbid",
         str_strip_whitespace=True,
     )
 
+    @classmethod
+    def as_form(
+        cls,
+        nome: str = Form(...),
+        tipo_arte: str = Form(...),
+        preco: float = Form(...),
+        imagem: UploadFile = File(...),
+    ):
+        return cls(
+            nome=nome,
+            tipo_arte=tipo_arte,
+            preco=preco,
+            imagem=imagem,
+        )
+
+class ImagensQuadrosResposta(BaseModel):
+
+    id_imagem_quadro: int
+    imagem: str
+
+    model_config = ConfigDict(
+        from_attributes=True,
+    )
 
 class ArteResposta(BaseModel):
 
@@ -34,6 +73,7 @@ class ArteResposta(BaseModel):
     tipo_arte: str
     preco: float
     id_usuario: int
+    imagem: ImagensQuadrosResposta
 
     model_config = ConfigDict(
         from_attributes=True,
